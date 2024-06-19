f98565959e6ca6a0ab286748663698d4619531ab
Marcelo Andrés <69094327+MARSFOREVER472@users.noreply.github.com>
2024-06-19 12:16:30 -0400

    Comienzo del proyecto!!!!!

---
+++ ArchivosEjecutables/index.js
@@ -0,0 +1,6 @@
+// CÓMO CREAR UN ARREGLO:
+
+const miArreglo = ["David", "Juan", "Kevin"]; // ARREGLO 1.
+const quantity = miArreglo.length; // CANTIDAD DE ELEMENTOS DENTRO DE UN ARREGLO.
+
+console.log(quantity);
---
+++ ArreglosYObjetos.js
@@ -0,0 +1,11 @@
+// CÓMO CREAR UN ARREGLO:
+
+const miArreglo = ["David", "Juan", "Kevin"]; // ARREGLO 1.
+
+// const miArreglo = ["David", 156, "Kevin"]; // ARREGLO INCORRECTO!!!!!!
+
+// PROPIEDAD "length" (Longitud):
+
+const quantity = miArreglo.length; // CANTIDAD DE ELEMENTOS DENTRO DE UN ARREGLO.
+
+console.log(quantity); // IMPRIME EL VALOR NÚMERO 3.
